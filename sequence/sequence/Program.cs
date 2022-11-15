d92f86ec2705aa48afefa8b159f0b562888b1838
Volha Belskaya <volha.belskaya.lt@gmail.com>
2022-11-15 13:32:58 +0200

    sequence

---
+++ sequence/sequence/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("a= ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b= ");
+int b = int.Parse(Console.ReadLine());
+Console.Read();
+
+for (int i = a; i <= b; i++)
+{
+    Console.Write(i);
+    Console.Write(", ");
+
+}
+Console.WriteLine();
